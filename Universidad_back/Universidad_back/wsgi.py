fde61db2cc2a9fbc1569241daf88af1dcb9d3895
marjimveg1 <maria-2696@hotmail.com>
2020-05-07 12:17:59 +0200

    pro

--- Universidad_back/Universidad_back/wsgi.py
+++ Universidad_back/Universidad_back/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Universidad_back.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Universidad_back.settings.production')
 
 application = get_wsgi_application()
