81bf6be751adfae3b2922ff79e7d2642ae06c769
marjimveg1 <maria-2696@hotmail.com>
2020-05-07 12:08:50 +0200

    metodo funcionando local

--- Universidad_back/Universidad_back/urls.py
+++ Universidad_back/Universidad_back/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Universidad_back.Apps.Gestion.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+path('inicio/', enviarCorreoPatadas()),
 ]
